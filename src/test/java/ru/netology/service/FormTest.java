8de523bd9ded4208cce424e761f410f68dd53c9f
Aliendas <127520548+Aliendas@users.noreply.github.com>
2023-07-11 15:07:44 +0500

    Update FormTest.java

--- src/test/java/ru/netology/service/FormTest.java
+++ src/test/java/ru/netology/service/FormTest.java
@@ -2,9 +2,12 @@ package ru.netology.service;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import dev.failsafe.internal.util.Durations;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
+
+import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -20,17 +23,17 @@ public class FormTest {
 
     @Test
     void shouldTestFormPositive() {
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
+        SelenideElement form = $("form.form");
         form.$("[data-test-id=name] input").setValue("Сергеева Марианна");
-        form.$("[data-test-id=phone] input").setValue("89367887878");
+        form.$("[data-test-id=phone] input").setValue("+79367887878");
         form.$("[data-test-id=agreement]").click();
-        form.$("button .button").click();
-        $("[data-test-id=order-success]").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        form.$("button.button").click();
+        $("[data-test-id='order-success']").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."), Duration.ofSeconds(10));
     }
 
     @Test
     void shouldTestNameNegative() {
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
+        SelenideElement form = $("form.form");
         form.$("[data-test-id=name] input").setValue("Marina Ivanova ");
         form.$("[data-test-id=phone] input").setValue("+79367887878");
         form.$("[data-test-id=agreement]").click();
@@ -40,16 +43,16 @@ public class FormTest {
 
     @Test
     void shouldTestWithoutName() {
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
+        SelenideElement form = $("form.form");
         form.$("[data-test-id=phone] input").setValue("+79367887878");
         form.$("[data-test-id=agreement]").click();
         form.$("button.button").click();
-        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения."));
+        $("[data-test-id='name'].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldTestPhoneNegative() {
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
+        SelenideElement form = $("form.form");
         form.$("[data-test-id=name] input").setValue("Сергеева Марианна");
         form.$("[data-test-id=phone] input").setValue("879367887878");
         form.$("[data-test-id=agreement]").click();
@@ -60,7 +63,7 @@ public class FormTest {
 
     @Test
     void shouldTestWithoutPhone() {
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
+        SelenideElement form = $("form.form");
         form.$("[data-test-id=name] input").setValue("Сергеева Марианна");
         form.$("[data-test-id=agreement]").click();
         form.$("button.button").click();
@@ -70,11 +73,11 @@ public class FormTest {
 
     @Test
     void shouldTestCheckbox() {
-        SelenideElement form = $("form form_size_m form_theme_alfa-on-white");
+        SelenideElement form = $("form.form");
         form.$("[data-test-id=name] input").setValue("Сергеева Марианна");
         form.$("[data-test-id=phone] input").setValue("+79367887878");
         form.$("button.button").click();
-        $("[data-test-id=agreement] .input_invalid").shouldBe(Condition.visible);
+        $("[data-test-id='agreement'] .input_invalid").shouldBe(Condition.visible);
 
     }
 }
